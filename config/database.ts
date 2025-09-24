f8d9f1fa322ced6c4d0fc10164e4933b5ddd127d
sergey-uhanov <uhanov.sergio@gmail.com>
2025-09-24 16:49:22 +0300

    1

--- config/database.ts
+++ config/database.ts
@@ -15,7 +15,6 @@ export default ({ env }) => {
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: fs.readFileSync(path.join(__dirname, '..', '..', env('DATABASE_SSL_CA'))).toString(),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
